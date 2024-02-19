f3932689da4b96e243d9da4e837d9538483abef6
coke5151 <houjunqimail@gmail.com>
2024-02-19 14:09:12 +0800

    create folder Zerojudge and move files

--- src/D483.java
+++
@@ -1,9 +0,0 @@
-package problems;
-
-public class D483 {
-
-	public static void main(String[] args) {
-		System.out.println("hello, world");
-	}
-
-}
---
+++ src/Zerojudge/D483.java
@@ -0,0 +1,9 @@
+package problems;
+
+public class D483 {
+
+	public static void main(String[] args) {
+		System.out.println("hello, world");
+	}
+
+}
