b44a7ad454f1eedb686217eadf8e3c38b96d5caf
pytree <houjunqimail@gmail.com>
2024-03-04 20:30:31 +0800

    fix syntax err in assignment one

--- src/assignment/EX_2_28.java
+++ src/assignment/EX_2_28.java
@@ -7,7 +7,7 @@ public class EX_2_28 {
 		try {
 			double r = in.nextDouble();
 			System.out.printf("diameter = %f\n", 2*r);
-			System.out.printf("circumference = %f\n", 2*Math.PI*r);;
+			System.out.printf("circumference = %f\n", 2*Math.PI*r);
 			System.out.printf("area = %f\n", Math.PI*r*r);	
 		} finally {
 			in.close();
